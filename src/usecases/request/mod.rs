5e0858c1ff6e036c77f73e49c8b1fa61a9a84b1e
Uwe Klotz <uwe.klotz@gmail.com>
2018-05-13 19:44:28 +0200

    Rename all enum literals consistently

--- src/usecases/request/mod.rs
+++ src/usecases/request/mod.rs
@@ -49,12 +49,10 @@ pub struct LocateParams {
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
-#[serde(deny_unknown_fields, rename_all = "lowercase")]
+#[serde(deny_unknown_fields, rename_all = "kebab-case")]
 pub enum ReplaceMode {
-    #[serde(rename = "update-only")]
     UpdateOnly,
 
-    #[serde(rename = "update-or-create")]
     UpdateOrCreate,
 }
 
