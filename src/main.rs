061deb0c1d0b82397b18f1aa78f070d1acf5a002
Uwe Klotz <uwe.klotz@gmail.com>
2019-03-11 23:23:53 +0100

    Fix deprecation warnings

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,7 @@ fn init_env_logger(log_level_filter: LogLevelFilter) {
     if env::var("RUST_LOG").is_ok() {
         let rust_log_var = &env::var("RUST_LOG").unwrap();
         println!("Parsing RUST_LOG={}", rust_log_var);
-        logger_builder.parse(rust_log_var);
+        logger_builder.parse_filters(rust_log_var);
     }
 
     logger_builder.init();
