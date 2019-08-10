d7da3dede91decff4b8ed5c362cbde9e7f4e7991
Uwe Klotz <uwe.klotz@gmail.com>
2019-08-11 00:17:29 +0200

    Move tests from domain into core

--- core/src/tag/tests.rs
+++ core/src/tag/tests.rs
@@ -18,6 +18,58 @@
 use super::*;
 
 #[test]
+fn score_valid() {
+    assert!(Score::min().validate().is_ok());
+    assert!(Score::max().validate().is_ok());
+    assert!(Score::new(Score::min().0 + Score::max().0)
+        .validate()
+        .is_ok());
+    assert!(!Score::new(Score::min().0 - Score::max().0)
+        .validate()
+        .is_ok());
+    assert!(!Score::new(Score::max().0 + Score::max().0)
+        .validate()
+        .is_ok());
+}
+
+#[test]
+fn score_display() {
+    assert_eq!("0.0%", format!("{}", Score::min()));
+    assert_eq!("100.0%", format!("{}", Score::max()));
+    assert_eq!("90.1%", format!("{}", Score(0.901_234_5)));
+    assert_eq!("90.2%", format!("{}", Score(0.901_5)));
+}
+
+#[test]
+fn parse_label() {
+    assert_eq!(
+        Ok(Label::new("A Label".into())),
+        "\tA Label  ".parse::<Label>()
+    );
+}
+
+#[test]
+fn validate_label() {
+    assert!(Label::new("A Term".into()).validate().is_ok());
+    assert!(Label::new("\tA Term  ".into()).validate().is_err());
+}
+
+#[test]
+fn parse_facet() {
+    assert_eq!(
+        Ok(Facet::new("a_facet".into())),
+        "\tA Facet  ".parse::<Facet>()
+    );
+}
+
+#[test]
+fn validate_facet() {
+    assert!(Facet::new("a_facet".into()).validate().is_ok());
+    assert!(Facet::new("a facet".into()).validate().is_err());
+    assert!(Facet::new("\tA facet  ".into()).validate().is_err());
+}
+
+#[test]
 fn default_facet_is_invalid() {
     assert!(Facet::default().validate().is_err());
 }
