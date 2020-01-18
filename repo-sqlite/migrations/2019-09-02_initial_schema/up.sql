5c45cec6ef13f3122d5fccc696ee6cf0287a8034
Uwe Klotz <uwe.klotz@gmail.com>
2020-01-18 20:42:30 +0100

    Reformat code

--- repo-sqlite/migrations/2019-09-02_initial_schema/up.sql
+++ repo-sqlite/migrations/2019-09-02_initial_schema/up.sql
@@ -20,7 +20,7 @@
 CREATE TABLE tbl_collection (
     id                       INTEGER PRIMARY KEY,
     uid                      BINARY(24) NOT NULL,
-    rev_no                  INTEGER NOT NULL,
+    rev_no                   INTEGER NOT NULL,
     rev_ts                   INTEGER NOT NULL,
     name                     TEXT NOT NULL,
     desc                     TEXT,
@@ -38,7 +38,7 @@ CREATE INDEX idx_collection_name ON tbl_collection (
 CREATE TABLE tbl_track (
     id                       INTEGER PRIMARY KEY,
     uid                      BINARY(24) NOT NULL,
-    rev_no                  INTEGER NOT NULL,
+    rev_no                   INTEGER NOT NULL,
     rev_ts                   INTEGER NOT NULL,
     data_fmt                 INTEGER NOT NULL,  -- serialization format
     data_vmaj                INTEGER NOT NULL,  -- serialization version for data migration - breaking changes
@@ -130,7 +130,7 @@ CREATE TABLE aux_track_brief (
     track_composer           TEXT,
     album_title              TEXT,
     album_artist             TEXT,
-    release_date          INTEGER,
+    release_date             INTEGER,
     track_number             INTEGER, -- > 0
     track_total              INTEGER, -- > 0
     disc_number              INTEGER, -- > 0
