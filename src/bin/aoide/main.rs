ab143ea41530350c51f65fc9ab85be982051f942
Uwe Klotz <uwe.klotz@gmail.com>
2020-03-08 22:57:16 +0100

    Tune SQLite settings

--- src/bin/aoide/main.rs
+++ src/bin/aoide/main.rs
@@ -66,9 +66,15 @@ fn create_connection_pool(url: &str, max_size: u32) -> Result<SqliteConnectionPo
 fn initialize_database(connection_pool: &SqliteConnectionPool) -> Result<(), Error> {
     log::info!("Initializing database");
     let connection = &*connection_pool.get()?;
-    sql_query("PRAGMA automatic_index=0;").execute(connection)?;
-    sql_query("PRAGMA foreign_keys=1;").execute(connection)?;
-    sql_query("PRAGMA encoding='UTF-8';").execute(connection)?;
+    sql_query(r#"
+PRAGMA journal_mode = WAL;        -- better write-concurrency
+PRAGMA synchronous = NORMAL;      -- fsync only in critical moments
+PRAGMA wal_autocheckpoint = 1000; -- write WAL changes back every 1000 pages (default), for an in average 1MB WAL file
+PRAGMA wal_checkpoint(TRUNCATE);  -- free some space by truncating possibly massive WAL files from the last run
+PRAGMA automatic_index = 1;       -- detect and log missing indexes
+PRAGMA foreign_keys = 1;          -- check foreign key constraints
+PRAGMA encoding = 'UTF-8';
+"#).execute(connection)?;
     Ok(())
 }
 
