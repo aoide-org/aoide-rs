5f805edd61160c0a5d165886a826edcac5d6b24c
Uwe Klotz <uwe.klotz@gmail.com>
2021-08-04 12:49:46 +0200

    Tune database settings

--- src/usecases/database.rs
+++ src/usecases/database.rs
@@ -29,9 +29,10 @@ pub fn initialize(connection: &SqliteConnection) -> Result<()> {
     log::info!("Initializing database");
     sql_query(r#"
 PRAGMA journal_mode = WAL;        -- better write-concurrency
-PRAGMA synchronous = NORMAL;      -- fsync only in critical moments
+PRAGMA synchronous = NORMAL;      -- fsync only in critical moments, safe for journal_mode = WAL
 PRAGMA wal_autocheckpoint = 1000; -- write WAL changes back every 1000 pages (default), for an in average 1MB WAL file
 PRAGMA wal_checkpoint(TRUNCATE);  -- free some space by truncating possibly massive WAL files from the last run
+PRAGMA secure_delete = 0;         -- avoid some disk I/O
 PRAGMA automatic_index = 1;       -- detect and log missing indexes
 PRAGMA foreign_keys = 1;          -- check foreign key constraints
 PRAGMA defer_foreign_keys = 1;    -- delay enforcement of foreign key constraints until commit
