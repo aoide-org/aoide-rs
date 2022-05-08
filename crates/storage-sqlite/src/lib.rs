cd169b67faccc3f72580f83570ebd81a8061cdfa
Uwe Klotz <uwe.klotz@gmail.com>
2022-05-08 11:24:07 +0200

    Disable rustflags/rustdocflags in Cargo config

--- crates/storage-sqlite/src/lib.rs
+++ crates/storage-sqlite/src/lib.rs
@@ -13,6 +13,20 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+// rustflags
+#![warn(rust_2018_idioms)]
+#![warn(rust_2021_compatibility)]
+#![warn(missing_debug_implementations)]
+#![warn(unreachable_pub)]
+#![warn(unsafe_code)]
+// rustflags (clippy)
+#![warn(clippy::all)]
+#![warn(clippy::explicit_deref_methods)]
+#![warn(clippy::explicit_into_iter_loop)]
+#![warn(clippy::explicit_iter_loop)]
+#![warn(clippy::must_use_candidate)]
+// rustdocflags
+#![warn(rustdoc::broken_intra_doc_links)]
 #![cfg_attr(not(test), deny(clippy::panic_in_result_fn))]
 #![cfg_attr(not(debug_assertions), deny(clippy::used_underscore_binding))]
 
