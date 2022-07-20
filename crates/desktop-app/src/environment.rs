50c521974e811378bfe059f078816a4326b71de5
Uwe Klotz <uwe.klotz@gmail.com>
2022-07-20 02:14:12 +0200

    desktop-app: Host the settings in the environment

--- crates/desktop-app/src/environment.rs
+++ crates/desktop-app/src/environment.rs
@@ -6,43 +6,55 @@ use std::{path::Path, sync::Arc};
 use aoide_backend_embedded::storage::DatabaseConfig;
 use aoide_storage_sqlite::connection::pool::gatekeeper::Gatekeeper;
 
-use crate::Settings;
+use crate::{settings, Settings};
 
 #[allow(missing_debug_implementations)]
 pub struct Environment {
     db_config: DatabaseConfig,
     db_gatekeeper: Arc<Gatekeeper>,
+    settings_state: Arc<settings::ObservableState>,
 }
 
 impl Environment {
-    pub fn restore_from_settings(settings: &Settings) -> anyhow::Result<Self> {
-        log::info!("Restoring environment from settings");
-        let db_config = settings.create_database_config()?;
-        let db_gatekeeper = Arc::new(aoide_backend_embedded::storage::provision_database(
-            &db_config,
-        )?);
+    /// Set up the runtime environment.
+    ///
+    /// The database configuration is created from the initial settings and
+    /// immutable. Switching the database connection and settings at runtime
+    /// is not supported.
+    pub fn commission(initial_settings: Settings) -> anyhow::Result<Self> {
+        log::info!("Commissioning runtime environment from settings");
+        let db_config = initial_settings.create_database_config()?;
+        let db_gatekeeper = aoide_backend_embedded::storage::provision_database(&db_config)?;
+        let settings_state = settings::ObservableState::new(initial_settings);
         Ok(Self {
             db_config,
-            db_gatekeeper,
+            db_gatekeeper: Arc::new(db_gatekeeper),
+            settings_state: Arc::new(settings_state),
         })
     }
 
+    /// Prepare for tear down.
+    ///
+    /// Rejects new database requests. Pending requests could still proceed
+    /// until finished.
+    pub fn decommission(&self) {
+        log::info!("Decommissioning runtime environment");
+        self.db_gatekeeper().decommission();
+    }
+
     #[must_use]
-    pub fn storage_dir(&self) -> Option<&Path> {
+    pub fn db_storage_dir(&self) -> Option<&Path> {
         self.db_config.connection.storage_dir()
     }
 
+    /// Access the database.
     #[must_use]
     pub fn db_gatekeeper(&self) -> &Arc<Gatekeeper> {
         &self.db_gatekeeper
     }
 
-    #[must_use]
-    pub fn db_gatekeeper_owned(&self) -> Arc<Gatekeeper> {
-        Arc::clone(self.db_gatekeeper())
-    }
-
-    pub fn abort_current_db_task(&self) {
-        self.db_gatekeeper.abort_current_task();
+    /// Manage the settings.
+    pub fn settings_state(&self) -> &Arc<settings::ObservableState> {
+        &self.settings_state
     }
 }
