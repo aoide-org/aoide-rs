0115b5f882c7affa42c2d346d94fe5d68d6df1e3
Uwe Klotz <uwe.klotz@gmail.com>
2022-07-20 20:02:21 +0200

    desktop-app: Remove settings from environment

--- crates/desktop-app/src/environment.rs
+++ crates/desktop-app/src/environment.rs
@@ -1,35 +1,27 @@
 // SPDX-FileCopyrightText: Copyright (C) 2018-2022 Uwe Klotz <uwedotklotzatgmaildotcom> et al.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use std::{path::Path, sync::Arc};
+use std::sync::Arc;
 
 use aoide_backend_embedded::storage::DatabaseConfig;
 use aoide_storage_sqlite::connection::pool::gatekeeper::Gatekeeper;
-
-use crate::{settings, Settings};
 
 #[allow(missing_debug_implementations)]
 pub struct Environment {
     db_config: DatabaseConfig,
     db_gatekeeper: Arc<Gatekeeper>,
-    settings_state: Arc<settings::ObservableState>,
 }
 
 impl Environment {
     /// Set up the runtime environment.
     ///
-    /// The database configuration is created from the initial settings and
-    /// immutable. Switching the database connection and settings at runtime
-    /// is not supported.
-    pub fn commission(initial_settings: Settings) -> anyhow::Result<Self> {
-        log::info!("Commissioning runtime environment from settings");
-        let db_config = initial_settings.create_database_config()?;
+    /// Modifying the database configuration at runtime is not supported.
+    pub fn commission(db_config: DatabaseConfig) -> anyhow::Result<Self> {
+        log::info!("Commissioning runtime environment");
         let db_gatekeeper = aoide_backend_embedded::storage::provision_database(&db_config)?;
-        let settings_state = settings::ObservableState::new(initial_settings);
         Ok(Self {
             db_config,
             db_gatekeeper: Arc::new(db_gatekeeper),
-            settings_state: Arc::new(settings_state),
         })
     }
 
@@ -42,20 +34,15 @@ impl Environment {
         self.db_gatekeeper().decommission();
     }
 
+    /// The database configuration.
     #[must_use]
-    pub fn db_storage_dir(&self) -> Option<&Path> {
-        self.db_config.connection.storage_dir()
+    pub fn db_config(&self) -> &DatabaseConfig {
+        &self.db_config
     }
 
     /// Access the database.
     #[must_use]
     pub fn db_gatekeeper(&self) -> &Arc<Gatekeeper> {
         &self.db_gatekeeper
-    }
-
-    /// Manage the settings.
-    #[must_use]
-    pub fn settings_state(&self) -> &Arc<settings::ObservableState> {
-        &self.settings_state
     }
 }
