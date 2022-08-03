b670865d2c061b5932f0f5f2348913d7c71e134c
Uwe Klotz <uwe.klotz@gmail.com>
2022-08-03 16:48:34 +0200

    Delete redundant newtype

--- crates/core-json/src/music/tempo/mod.rs
+++ crates/core-json/src/music/tempo/mod.rs
@@ -1,29 +1,4 @@
 // SPDX-FileCopyrightText: Copyright (C) 2018-2022 Uwe Klotz <uwedotklotzatgmaildotcom> et al.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use crate::prelude::*;
-
-mod _core {
-    pub(super) use aoide_core::music::tempo::*;
-}
-
-///////////////////////////////////////////////////////////////////////
-// Tempo
-///////////////////////////////////////////////////////////////////////
-
-#[derive(Debug, Serialize, Deserialize)]
-#[cfg_attr(test, derive(PartialEq))]
-#[cfg_attr(feature = "schemars", derive(JsonSchema))]
-pub struct TempoBpm(_core::Bpm);
-
-impl From<_core::TempoBpm> for TempoBpm {
-    fn from(from: _core::TempoBpm) -> Self {
-        Self(from.to_raw())
-    }
-}
-
-impl From<TempoBpm> for _core::TempoBpm {
-    fn from(from: TempoBpm) -> Self {
-        Self::from_raw(from.0)
-    }
-}
+pub type TempoBpm = aoide_core::music::tempo::TempoBpm;
