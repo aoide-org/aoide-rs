9ec4bb7bbdbe62eafc926dd6ac74ad189045bce6
Uwe Klotz <uwe.klotz@gmail.com>
2022-08-11 10:41:47 +0200

    Add album view(s) to the SQLite schema

---
+++ crates/repo-sqlite/migrations/0011_view_album/up.sql
@@ -0,0 +1,30 @@
+-- SPDX-FileCopyrightText: Copyright (C) 2018-2022 Uwe Klotz <uwedotklotzatgmaildotcom> et al.
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+DROP VIEW IF EXISTS view_album;
+CREATE VIEW view_album AS
+SELECT
+track.album_kind AS kind,
+track.publisher AS publisher,
+album_artist.name AS artist,
+album_title.name AS title,
+MIN(recorded_at_yyyymmdd) AS min_recorded_at_yyyymmdd,
+MAX(recorded_at_yyyymmdd) AS max_recorded_at_yyyymmdd,
+MIN(released_at_yyyymmdd) AS min_released_at_yyyymmdd,
+MAX(released_at_yyyymmdd) AS max_released_at_yyyymmdd,
+MIN(disc_number) AS min_disc_number,
+MAX(disc_number) AS max_disc_number,
+COUNT(track.row_id) AS track_count,
+GROUP_CONCAT(track.row_id) AS track_id_concat
+FROM track
+JOIN track_actor AS album_artist ON track.row_id=album_artist.track_id AND album_artist.scope=1 AND album_artist.kind=0 AND album_artist.role=0
+JOIN track_title AS album_title ON track.row_id=album_title.track_id AND album_title.scope=1 AND album_title.kind=0
+GROUP BY track.album_kind,track.publisher,album_artist.name,album_title.name;
+
+DROP VIEW IF EXISTS view_album_artist_initial;
+CREATE VIEW view_album_artist_initial AS
+SELECT
+upper(substr(artist,1,1)) AS artist_initial,
+count(*) AS album_count
+FROM view_album
+GROUP BY artist_initial;
