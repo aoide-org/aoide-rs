9d6a581fef0ffa8e78e485c32b921257f67d88f1
Uwe Klotz <uwe.klotz@gmail.com>
2023-01-15 23:19:23 +0100

    media: Double check file types on metadata export

--- crates/media/src/io/export.rs
+++ crates/media/src/io/export.rs
@@ -1,9 +1,13 @@
 // SPDX-FileCopyrightText: Copyright (C) 2018-2023 Uwe Klotz <uwedotklotzatgmaildotcom> et al.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use std::{fs::OpenOptions, path::Path};
+use std::{
+    fs::{File, OpenOptions},
+    path::Path,
+};
 
 use bitflags::bitflags;
+use lofty::FileType;
 
 use aoide_core::track::{
     actor::{Actor, Actors, Kind as ActorKind, Role as ActorRole},
@@ -48,28 +52,72 @@ pub fn export_track_to_path(
     config: &ExportTrackConfig,
     track: &mut Track,
 ) -> Result<bool> {
-    match track.media_source.content.r#type.essence_str() {
-        "audio/aiff" => {
+    let file = File::open(path)?;
+    let probe = lofty::Probe::new(file).guess_file_type()?;
+    let Some(file_type) = probe.file_type() else {
+        log::debug!(
+            "Skipping export of track {media_source_content_link:?}: {config:?}",
+            media_source_content_link = track.media_source.content.link
+        );
+        return Err(Error::UnsupportedContentType(
+            track.media_source.content.r#type.clone(),
+        ));
+    };
+    match file_type {
+        FileType::AIFF => {
+            if track.media_source.content.r#type.essence_str() != "audio/aiff" {
+                return Err(Error::UnsupportedContentType(
+                    track.media_source.content.r#type.clone(),
+                ));
+            }
             let mut file = OpenOptions::new().write(true).open(path)?;
             crate::fmt::aiff::export_track_to_file(&mut file, config, track)
         }
-        "audio/flac" => {
+        FileType::FLAC => {
+            if track.media_source.content.r#type.essence_str() != "audio/flac" {
+                return Err(Error::UnsupportedContentType(
+                    track.media_source.content.r#type.clone(),
+                ));
+            }
             let mut file = OpenOptions::new().write(true).open(path)?;
             crate::fmt::flac::export_track_to_file(&mut file, config, track)
         }
-        "audio/mpeg" => {
-            let mut file = OpenOptions::new().write(true).open(path)?;
-            crate::fmt::mpeg::export_track_to_file(&mut file, config, track)
-        }
-        "audio/m4a" | "video/mp4" => {
+        FileType::MP4 => {
+            if !matches!(
+                track.media_source.content.r#type.essence_str(),
+                "audio/m4a" | "audio/mp4" | "video/mp4"
+            ) {
+                return Err(Error::UnsupportedContentType(
+                    track.media_source.content.r#type.clone(),
+                ));
+            }
             let mut file = OpenOptions::new().write(true).open(path)?;
             crate::fmt::mp4::export_track_to_file(&mut file, config, track)
         }
-        "audio/ogg" => {
+        FileType::MPEG => {
+            if track.media_source.content.r#type.essence_str() != "audio/mpeg" {
+                return Err(Error::UnsupportedContentType(
+                    track.media_source.content.r#type.clone(),
+                ));
+            }
+            let mut file = OpenOptions::new().write(true).open(path)?;
+            crate::fmt::mpeg::export_track_to_file(&mut file, config, track)
+        }
+        FileType::Opus => {
+            if track.media_source.content.r#type.essence_str() != "audio/ogg" {
+                return Err(Error::UnsupportedContentType(
+                    track.media_source.content.r#type.clone(),
+                ));
+            }
             let mut file = OpenOptions::new().write(true).open(path)?;
             crate::fmt::ogg::export_track_to_file(&mut file, config, track)
         }
-        "audio/opus" => {
+        FileType::Vorbis => {
+            if track.media_source.content.r#type.essence_str() != "audio/opus" {
+                return Err(Error::UnsupportedContentType(
+                    track.media_source.content.r#type.clone(),
+                ));
+            }
             let mut file = OpenOptions::new().write(true).open(path)?;
             crate::fmt::opus::export_track_to_file(&mut file, config, track)
         }
