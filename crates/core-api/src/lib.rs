0fbf630da7558d3eaf1b01ad2c4fe3b249206eab
Uwe Klotz <uwe.klotz@gmail.com>
2023-07-29 00:00:10 +0200

    Do not allow self named module files

--- crates/core-api/src/lib.rs
+++ crates/core-api/src/lib.rs
@@ -8,7 +8,9 @@
 #![warn(unsafe_code)]
 #![warn(rustdoc::broken_intra_doc_links)]
 #![warn(clippy::pedantic)]
+// Additional restrictions
 #![warn(clippy::clone_on_ref_ptr)]
+#![warn(clippy::self_named_module_files)]
 // Repetitions of module/type names occur frequently when using many
 // modules for keeping the size of the source files handy. Often
 // types have the same name as their parent module.
