859bbdc9b4bfa83a3d590e9850be292122ccd6f8
Uwe Klotz <uwe.klotz@gmail.com>
2024-01-31 13:32:07 +0100

    Add demo desktop application based on Vizia

---
+++ demo-app/src/main.rs
@@ -0,0 +1,135 @@
+// SPDX-FileCopyrightText: Copyright (C) 2018-2024 Uwe Klotz <uwedotklotzatgmaildotcom> et al.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// Required for `#[derive(Lens)]`!?
+#![allow(clippy::expl_impl_clone_on_copy)]
+
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+use directories::ProjectDirs;
+use vizia::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let Some(config_dir) = app_config_dir() else {
+        log::error!("Config directory is unavailable");
+        return;
+    };
+
+    if !config_dir.exists() {
+        log::error!(
+            "Config directory '{dir_path}' does not exist",
+            dir_path = config_dir.display()
+        );
+        return;
+    }
+
+    match config_dir
+        .metadata()
+        .map(|metadata| metadata.permissions().readonly())
+    {
+        Ok(readonly) => {
+            if readonly {
+                log::warn!(
+                    "Config directory (read-only): {dir_path}",
+                    dir_path = config_dir.display()
+                );
+            } else {
+                log::info!(
+                    "Config directory: {dir_path}",
+                    dir_path = config_dir.display()
+                );
+            }
+        }
+        Err(err) => {
+            log::error!("Failed to query permissions of config directory: {err}");
+        }
+    };
+
+    let aoide_initial_settings =
+        match aoide::desktop_app::settings::State::restore_from_parent_dir(&config_dir) {
+            Ok(settings) => settings,
+            Err(err) => {
+                log::error!("Failed to restore aoide settings: {err}");
+                return;
+            }
+        };
+    let aoide_db_config = match aoide_initial_settings.create_database_config() {
+        Ok(db_config) => db_config,
+        Err(err) => {
+            log::error!("Failed to create aoide database config: {err}");
+            return;
+        }
+    };
+    log::debug!("Commissioning aoide library backend: {aoide_db_config:?}");
+    let aoide_handle = match aoide::desktop_app::Handle::commission(&aoide_db_config) {
+        Ok(library_backend) => library_backend,
+        Err(err) => {
+            log::error!("Failed to commission aoide library backend: {err}");
+            return;
+        }
+    };
+
+    Application::new(|cx| {
+        AppData {
+            config_dir,
+            aoide_handle,
+        }
+        .build(cx);
+
+        Label::new(
+            cx,
+            AppData::config_dir.map(|config_dir| {
+                format!(
+                    "Config dir: {config_dir}",
+                    config_dir = config_dir.display()
+                )
+            }),
+        );
+    })
+    .title(app_name())
+    .run();
+}
+
+#[must_use]
+const fn app_name() -> &'static str {
+    env!("CARGO_PKG_NAME")
+}
+
+#[must_use]
+fn app_dirs() -> Option<ProjectDirs> {
+    ProjectDirs::from("", "", app_name())
+}
+
+#[must_use]
+fn init_config_dir(app_dirs: &ProjectDirs) -> &Path {
+    let app_config_dir = app_dirs.config_dir();
+    if let Err(err) = fs::create_dir_all(app_config_dir) {
+        log::warn!(
+            "Failed to create config directory '{dir}': {err}",
+            dir = app_config_dir.display(),
+        );
+    }
+    app_config_dir
+}
+
+fn app_config_dir() -> Option<PathBuf> {
+    app_dirs()
+        .as_ref()
+        .map(init_config_dir)
+        .map(Path::to_path_buf)
+}
+
+#[derive(Lens)]
+#[allow(missing_debug_implementations)]
+struct AppData {
+    config_dir: PathBuf,
+    aoide_handle: aoide::desktop_app::Handle,
+}
+
+impl Model for AppData {}
