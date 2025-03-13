ec1adfafb65c774a1729c6a95054ece266afa0a7
Uwe Klotz <uwe.klotz@gmail.com>
2025-03-13 14:51:54 +0100

    db: Re-create the collection table with unqiue indexes
    The previous schema definition considered NULL values in the column
    `kind` as distinct. All indexes are now named and could be altered
    independent of the referenced table.
    
    See also: https://www.sqlite.org/nulls.html

---
+++ crates/repo-sqlite/migrations/0015_collection/up.sql
@@ -0,0 +1,71 @@
+-- SPDX-FileCopyrightText: Copyright (C) 2018-2025 Uwe Klotz <uwedotklotzatgmaildotcom> et al.
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+-- Rename and re-create the table as proposed here: https://www.sqlite.org/lang_altertable.html
+
+-- !!!This pragma is a no-op within a transaction!!!
+-- Migrations are usually run within a transaction.
+PRAGMA foreign_keys = OFF;
+
+CREATE TABLE collection_migrate (
+    -- row header (immutable)
+    row_id                 INTEGER PRIMARY KEY,
+    row_created_ms         INTEGER NOT NULL,
+    -- row header (mutable)
+    row_updated_ms         INTEGER NOT NULL,
+    -- entity header (immutable)
+    entity_uid             TEXT NOT NULL, -- ULID
+    -- entity header (mutable)
+    entity_rev             INTEGER NOT NULL, -- RevisionNumber
+    -- properties
+    title                  TEXT NOT NULL,
+    kind                   TEXT,
+    notes                  TEXT,
+    color_rgb              INTEGER, -- 0xRRGGBB (hex)
+    color_idx              INTEGER, -- palette index
+    media_source_path_kind INTEGER NOT NULL,
+    media_source_root_url  TEXT
+) STRICT;
+INSERT INTO collection_migrate SELECT * FROM collection;
+DROP TABLE collection;
+ALTER TABLE collection_migrate RENAME TO collection;
+
+-- Verify that all foreign key constraints are still valid.
+PRAGMA foreign_key_check;
+
+-- !!!This pragma is a no-op within a transaction!!!
+-- Migrations are usually run within a transaction.
+PRAGMA foreign_keys = ON;
+
+-- Only the last revision is stored.
+CREATE UNIQUE INDEX udx_collection_entity_uid ON collection (
+    entity_uid
+);
+
+-- NULL values are considered as distinct for UNIQUE indexes.
+-- Therefore we need to maintain 2 separate partial UNIQUE indexes to enforce the desired constraints.
+--
+-- Creating the first partial index may fail if conflicting titles without a kind (= NULL) exist.
+-- In this case the affected collections need to be deleted manually.
+--
+-- See also:
+--  - https://www.sqlite.org/nulls.html
+--  - https://www.sqlite.org/partialindex.html
+CREATE UNIQUE INDEX udx_collection_title_where_kind_null ON collection (
+    title
+) WHERE kind IS NULL;
+CREATE UNIQUE INDEX udx_collection_kind_title ON collection (
+    kind, title
+) WHERE kind IS NOT NULL;
+
+CREATE INDEX idx_collection_row_created_ms_desc ON collection (
+    row_created_ms DESC
+);
+
+CREATE INDEX idx_collection_row_updated_ms_desc ON collection (
+    row_updated_ms DESC
+);
+
+CREATE INDEX idx_collection_title ON collection (
+    title
+);
--- crates/usecases-sqlite/src/database.rs
+++ crates/usecases-sqlite/src/database.rs
@@ -1,16 +1,33 @@
 // SPDX-FileCopyrightText: Copyright (C) 2018-2025 Uwe Klotz <uwedotklotzatgmaildotcom> et al.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use diesel::RunQueryDsl as _;
+
 use aoide_repo_sqlite::{DbConnection, run_migrations};
 
 use crate::{Error, Result};
 
 pub fn migrate_schema(connection: &mut DbConnection) -> Result<()> {
+    // Some migrations require to temporarily disable foreign key constraints.
+    // This cannot be accomplished in a migrations script that is executed
+    // within a transaction scope.
+    // See also: https://www.sqlite.org/pragma.html#pragma_foreign_keys
+    diesel::sql_query("PRAGMA foreign_keys=OFF;").execute(connection)?;
+    let mut foreign_key_check_after_running_migrations = false;
     for migration_version in run_migrations(connection)
         .map_err(|err| anyhow::anyhow!(err))
         .map_err(Error::DatabaseMigration)?
     {
         log::info!("Applied migration '{migration_version}'");
+        foreign_key_check_after_running_migrations = true;
     }
+    // Check foreign key constraints after running one or more migrations
+    // with foreign key constraints disabled to ensure integrity of the
+    // database.
+    if foreign_key_check_after_running_migrations {
+        diesel::sql_query("PRAGMA foreign_key_check;").execute(connection)?;
+    }
+    // Re-enable foreign key constraints.
+    diesel::sql_query("PRAGMA foreign_keys=ON;").execute(connection)?;
     Ok(())
 }
