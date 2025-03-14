9fcc5548701f7f3d440773f8f4a3a9e68fd31f0f
Uwe Klotz <uwe.klotz@gmail.com>
2025-03-15 00:37:13 +0100

    Remove non-working foreign key check

--- crates/usecases-sqlite/src/database.rs
+++ crates/usecases-sqlite/src/database.rs
@@ -12,22 +12,14 @@ pub fn migrate_schema(connection: &mut DbConnection) -> Result<()> {
     // This cannot be accomplished in a migrations script that is executed
     // within a transaction scope.
     // See also: https://www.sqlite.org/pragma.html#pragma_foreign_keys
-    diesel::sql_query("PRAGMA foreign_keys=OFF;").execute(connection)?;
-    let mut foreign_key_check_after_running_migrations = false;
+    diesel::sql_query("PRAGMA foreign_keys=OFF").execute(connection)?;
     for migration_version in run_migrations(connection)
         .map_err(|err| anyhow::anyhow!(err))
         .map_err(Error::DatabaseMigration)?
     {
         log::info!("Applied migration '{migration_version}'");
-        foreign_key_check_after_running_migrations = true;
-    }
-    // Check foreign key constraints after running one or more migrations
-    // with foreign key constraints disabled to ensure integrity of the
-    // database.
-    if foreign_key_check_after_running_migrations {
-        diesel::sql_query("PRAGMA foreign_key_check;").execute(connection)?;
     }
     // Re-enable foreign key constraints.
-    diesel::sql_query("PRAGMA foreign_keys=ON;").execute(connection)?;
+    diesel::sql_query("PRAGMA foreign_keys=ON").execute(connection)?;
     Ok(())
 }
